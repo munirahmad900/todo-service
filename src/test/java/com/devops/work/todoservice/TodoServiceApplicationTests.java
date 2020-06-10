b07e441cebfc2a06072cdade3f76943393d59120
Munir <munir.ahmad900@gmail.com>
2020-06-10 16:30:03 +0500

    first commit

---
+++ src/test/java/com/devops/work/todoservice/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.work.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
